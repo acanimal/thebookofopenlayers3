17a1ae9e1e9ab56110b4d309edc6eff12052c766
acanimal <asantiago@acuriousanimal.com>
2014-08-07 23:23:32 +0200

    General samples structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	var SAMPLES_FILE = 'samples.json';
+	var THUMBNAIL_FILE = '/thumbnail.png';
 
 	// Loads chapter summary data and create elements
 	var loadChaptersSummary = function(data) {
@@ -12,11 +13,11 @@
 
 			for(si=0; si<chapter.samples.length; si++) {
 				sample = chapter.samples[si];
-				imgsrc = sample.thumbnail ? sample.thumbnail : 'holder.js/240x180/text:No Preview';
+				imgsrc = sample.thumbnail ? sample.url + THUMBNAIL_FILE : 'holder.js/240x180/text:No Preview';
 
 				el = '<div class="sample col-xs-6 col-md-4 col-lg-3">' +
                     '<h5>'+ sample.title+'</h5>'+
-                    '<a href="'+ location.href+'?load='+encodeURIComponent(sample.file) +'">'+
+                    '<a href="'+ sample.url +'">'+
                     '<img data-src="'+ imgsrc +'" width="180" height="100">'+
                     '</a>'+
                     '<p class="small">'+ sample.description +'</p>'+
@@ -29,18 +30,6 @@
 
 		var samplesContainer = $('#samples-container');
 		samplesContainer.append(elems);
-	};
-
-	// Get URL paramters by name
-	function getParameterByName(name) {
-		name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-		var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'), results = regex.exec(location.search);
-		return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-	}
-
-	// Load sample
-	var loadSample = function(file) {
-		file=0;
 	};
 
 	// Load specified file and invoke callback if success
@@ -63,13 +52,7 @@
 			message.html('Sorry there was an error loading the <code>'+url+'</code> file !!!');
 		});
 	};
-
-	// Check if we need to load the samples summary or a concrete example
-	var file = decodeURIComponent(getParameterByName('load'));
-	if(!file) {
-		loadFile(SAMPLES_FILE, loadChaptersSummary);
-	} else {
-		loadFile(file, loadSample);
-	}
+	
+	loadFile(SAMPLES_FILE, loadChaptersSummary);
 	
 })();
